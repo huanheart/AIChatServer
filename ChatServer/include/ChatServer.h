0f6d654c1a44a69428de76d4398f2c3150aaa370
lkt <luokaitai306@gmail.com>
2025-09-07 23:55:43 +0800

    修复问题

--- ChatServer/include/ChatServer.h
+++ ChatServer/include/ChatServer.h
@@ -26,7 +26,7 @@ class ChatServer {
 public:
 	ChatServer(int port,
 		const std::string& name,
-		muduo::net::TcpServer::Option option);
+		muduo::net::TcpServer::Option option = muduo::net::TcpServer::kNoReusePort);
 
 	void setThreadNum(int numThreads);
 	void start();
@@ -64,6 +64,9 @@ private:
 	std::unordered_map<int, bool>	onlineUsers_;
 	std::mutex	mutexForOnlineUsers_;
 	//每一个人都有对应的对话 userid->AIHelper
+	//注意：存放指针是因为后续需要对chatInformation[key]进行更改
+	//若直接存放对象，不存放指针，那么由于unordered_map不是线程安全的，需要加锁对其
+	//里面的vector进行插入操作，那么用户A访问AI的操作就会严重影响到用户B
 	std::unordered_map<int, std::shared_ptr<AIHelper>> chatInformation;
 	std::mutex	mutexForChatInformation;
 };
